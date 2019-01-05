e3f22e150347e1c34d727f7a687c147d75ba2132
Baklanov Alexander P <alexzonder@e1.ru>
2019-01-05 15:00:40 +0500

    mistype

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,7 +9,7 @@ class CommandTests(TestCase):
 
     def test_wait_for_ready(self):
         """ Test waiting for db when db is available """
-        with patch('django.db.utils.ConnectionHandler.__getitem__'), as gi:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
@@ -21,4 +21,3 @@ class CommandTests(TestCase):
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
-            
